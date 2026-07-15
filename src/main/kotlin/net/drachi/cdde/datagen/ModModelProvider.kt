c470b449add18adc8f73d8bcb40f9453ab345b64
GoldenDrachi <GoldenDrachi@gmail.com>
2026-07-15 13:34:03 +0200

    Fixing missing models

--- src/main/kotlin/net/drachi/cdde/datagen/ModModelProvider.kt
+++ src/main/kotlin/net/drachi/cdde/datagen/ModModelProvider.kt
@@ -20,13 +20,9 @@ class ModModelProvider(output: FabricDataOutput) : FabricModelProvider(output) {
         blockStateModelGenerator.createTrivialCube(ModBlocks.MINION_SPAWN)
         blockStateModelGenerator.createTrivialCube(ModBlocks.TREASURE_DOOR)
         blockStateModelGenerator.createTrivialCube(ModBlocks.BOSS_DOOR)
-        blockStateModelGenerator.createTrivialCube(ModBlocks.HAZARD_WALL_WATER)
-        blockStateModelGenerator.createTrivialCube(ModBlocks.HAZARD_WALL_LAVA)
-        blockStateModelGenerator.createTrivialCube(ModBlocks.HAZARD_WALL_VOID)
     }
 
     override fun generateItemModels(itemModelGenerator: ItemModelGenerators) {
         // Automatically handled by createTrivialCube for simple blocks
-        itemModelGenerator.generateFlatItem(net.drachi.cdde.registry.ModItems.TREASURE_KEY, net.minecraft.data.models.model.ModelTemplates.FLAT_ITEM)
     }
 }
